e7d9c141ef3460ffe107cfecde745ba5e2f3de73
moseskereya <lazaromoses77@gmail.com>
2019-06-23 20:33:57 +0300

    modification of index page and main css and script js page

--- mainscripts/main.js
+++ mainscripts/main.js
@@ -17,22 +17,16 @@
 
     const btns = document.querySelectorAll('.btn');
     const screen = document.querySelector('.screen');
-    
     for (let i = 0; i < btns.length; i++){
     btns[i].addEventListener('click', function (){
     let number = btns[i].getAttribute('data-num');
     screen.value = number;
     })
     }
-    
+
     const clearBtn = document.querySelector('.clear')
     function myFunction(){
       screen.value = '';
-      if(screen.value === ''){
-        alert('please the select input has to be filled')
-      }else{
-        alert('Dear customer are you sure you want to delet the item you selected?')
-      }
     }
 
 
